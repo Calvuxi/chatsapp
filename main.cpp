c8e99811120598eec147f1ab41a40f4cc6be9028
David Pérez <hallaplay835@gmail.com>
2014-04-18 00:52:32 +0200

    Esqueletos de los módulos.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* --------------- AUTHORS ---------------
+- David Pérez (hallaplay835@gmail.com).
+- Iván Calvo (calvuxi@gmail.com).
+*/
+
+// #### Librerías de sistema ####
+
+// #### UDLs ####
+
+// #### Namespaces ####
+
+// #### Constantes ####
+
+// #### Declaraciones typedef ####
+
+// #### Prototipos ####
+
+// #### Implementaciones ####
