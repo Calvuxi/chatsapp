b942301277cd6c969efe8c517f12070a83d898e0
David Pérez <hallaplay835@gmail.com>
2014-04-19 00:58:21 +0200

    Añade config.h (2b0526d41957f8d9d78dbe6c7aa7b93c9ba27e2b no lo hizo). Descomenta las declaraciones de los elementos de la estructura tMensaje, que causaban excepción en ejecución. Por alguna extraña razón si MAX_MENSAJES se pone a más de 206 la estructura causa una excepción en ejecución.

---
+++ config.h
@@ -0,0 +1,30 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+/* --------------- AUTHORS ---------------
+- David Pérez (hallaplay835@gmail.com).
+- Iván Calvo (calvuxi@gmail.com).
+*/
+
+// #### Librerías de sistema ####
+#include <string>
+
+// #### UDLs ####
+
+// #### Namespaces ####
+using namespace std;
+
+// #### Constantes ####
+const unsigned short MAX_USUARIOS = 50;
+const unsigned short MIN_USER_LENGTH = 4;
+const unsigned short MAX_USER_LENGTH = 20;
+const unsigned short MAX_MENSAJES = 206; // Da una excepción en ejecución si se pone a más de 206.
+const unsigned short MAX_CHATS = 50;
+
+const string CENTINELA = "XXX";
+const string USER_LIST = "usuarios.txt";
+
+// #### Declaraciones typedef ####
+
+// #### Prototipos ####
+
+#endif
